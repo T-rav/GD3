3165b524895e55c727b86cd28ccce3ab9e9feba7
T-rav <tmfrisinger@gmail.com>
2018-07-03 11:09:19 +0200

    Added todo comments with metrics to calculate

---
+++ Analysis.Tests/Learning.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Analysis.Tests
+{
+    [TestFixture]
+    public class Learning
+    {
+        // Time To 100 Lines
+        https://help.gitprime.com/using-the-app/calculations/what-is-tt100
+
+        // New v Old v Help v Churn
+        // https://help.gitprime.com/using-the-app/calculations/what-is-new-work-vs-other-work
+
+        // Developer, Rank, Active Days, Commits Per Day, Efficency, Impact
+        // Effiecney is function of churn
+
+        // https://help.gitprime.com/using-the-app/calculations/what-is-impact
+        // Impact is calculated by number of files changed in a commit (Roughly how much cognitive load did the engineer carry when implementing these changes?)
+        [Test]
+        public void METHOD()
+        {
+            // arrange
+
+            // act
+
+            // assert
+        }
+    }
+}
---
+++ source/GD3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GD3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
