ecb959b94c7f2727ca0a8b309bf3d34df517a6d6
T-rav <tmfrisinger@gmail.com>
2018-07-05 10:32:17 +0200

    Added Active Days Per Week Metric

--- scripts/learning.sh
+++ scripts/learning.sh
@@ -20,6 +20,13 @@ function pad(){
     done
 }
 
+function ceiling () {
+  DIVIDEND=${1}
+  DIVISOR=${2}
+  RESULT=$(( ( ( ${DIVIDEND} - ( ${DIVIDEND} % ${DIVISOR}) )/${DIVISOR} ) + 1 ))
+  echo $RESULT
+}
+
 if [ "$#" -lt 1 ]; then
     echo "Illegal number of parameters - please pass the git repository path!"
     exit
@@ -29,6 +36,7 @@ fi
 gitDirctory=$1
 currentDirctory=$(pwd)
 totalWorkingDays=$(calculateWorkingDays)
+totalWorkingWeeks=$(ceiling $totalWorkingDays 5)
 
 dataPath="$currentDirctory/data.txt"
 rawCommitStats="$currentDirctory/rawCommitStats.txt"
@@ -50,25 +58,37 @@ awk -v days="$totalWorkingDays" '{ arr[$3]+=$1 } END {for (key in arr) printf("%
 grep -- -- < $dataPath | awk -F'--' '{print $3" "$4}' | sort | uniq | cut -d' ' -f2 | sort | uniq -c > $activeDaysPerDeveloper
 
 # --- Print Dashboard ---
-echo -e "\e[96mGD3 Stats - v0.9.1\e[39m"
+echo -e "\e[96mGD3 Stats - v0.9.2\e[39m"
 echo -e "\e[93mFor 2018-06-25 - $(date +%Y-%m-%d)\e[39m"
-echo "---------------------------------------------------------"
-echo -e "Developer      | Active Days   | Commits / Day | Impact" 
-echo  "---------------------------------------------------------"
+echo "-----------------------------------------------------------------------------------"
+echo -e "Developer      | Active Days | Active Days Per Week* | Commits / Day | Impact" 
+echo  "---------------------------------------------------------------------------------"
 
 while read line
 do
     developer=$(echo $line | cut -d' ' -f2)
     activeDays=$(echo $line | cut -d' ' -f1)
+    activeDaysPerWeek=$(( $activeDays*10 / $totalWorkingWeeks*10))
+    deciamlActiveDaysPerWeek=$(echo $activeDaysPerWeek | sed 's/..$/.&/')
     commitsPerWorkingDay=$(grep "$developer" < $individualCommitStats | awk -F'\t' '{print $3}' )
+    
     echo -n "$developer"
     pad ${#developer} 17 
+
     echo -n "$activeDays"
-    pad ${#activeDays} 16
+    pad ${#activeDays} 14
+
+    echo -n "$deciamlActiveDaysPerWeek"
+    pad ${#deciamlActiveDaysPerWeek} 24
+
     echo -n "$commitsPerWorkingDay"
     pad ${#commitsPerWorkingDay} 16
+
     echo "todo"
 done < $activeDaysPerDeveloper
+
+echo ""
+echo "* Average is 3.2 days per week"
 
 # clean up
 rm $dataPath $rawCommitStats $individualCommitStats $activeDaysPerDeveloper
