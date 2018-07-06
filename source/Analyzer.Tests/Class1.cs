98f91c275bc92b3b5969cadee73d74cf810aaf61
T-rav <tmfrisinger@gmail.com>
2018-07-06 14:08:25 +0200

    Started dotnet core implementation of stats parsing stuff

---
+++ source/Analyzer.Tests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using LibGit2Sharp;
+using NUnit.Framework;
+
+namespace Analyzer.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void LearningTest_Goal_ReadGitHistory()
+        {
+            // arrange
+            var repoPath = "C:\\Systems\\pdf-poc";
+            var repo = new Repository(repoPath);
+            // act
+            var commits = repo.Head.Commits;
+            // assert
+            commits.Should().NotBeNull();
+        }
+    }
+}
