01309b34cad88011e1364c464b875d1c4a3d9b72
T-rav <tmfrisinger@gmail.com>
2018-10-15 08:31:05 +0200

    devOps: added version.txt and included release.sh which builds nuget package

--- source/DevOps/release.sh
+++ source/DevOps/release.sh
@@ -1,9 +1,43 @@
 #!/bin/sh
 
-workingDir=".."
+#move up to start the process
+cd ..
 
-cd $workingDir
+netcoreVersion="2.1"
+version=$(cat version.txt | cut -d. -f1,2,3)
+build=$(cat version.txt | cut -d. -f4)
+build=$(($build+1))
 
+echo -n "$version.$build" > version.txt
+
+export Version=$(cat version.txt)
+echo -e "Setting version to \e[32m$Version\e[0m"
+echo -e "\e[33mTargeting dotnetcore version \e[35m$netcoreVersion\e[0m"
 dotnet restore
+dotnet clean
 dotnet build
-dotnet publish -c Release -r win10-x64
+
+echo -e "\e[35mRunning test for solution...\e[0m"
+for project in *.Tests/*.csproj; do 
+	echo -e "\e[33Executing Tests for $project\e[0m"
+	dotnet test --no-build $project  
+	if [ $? -ne 0 ] 
+	then
+		echo -e "\e[31mTest Failure, Aborting!!!\e[0m"
+		git reset --hard
+		exit
+	fi
+done
+
+if [ ! -d "dist" ]; then
+  mkdir dist
+fi
+
+echo -e "\e[33mCreating NuGet Package\e[0m"
+dotnet pack -c Release --output dist
+
+# commit build number bump to source
+cd ..
+git add version.txt
+git commit -m "devOps(release):version bump to [$version.$build] from release script"
+git push
