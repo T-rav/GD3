f34eabd6e51324d8cb688f06fb8fb9bbb2da85dd
T-rav <tmfrisinger@gmail.com>
2018-10-15 10:35:56 +0200

    devOps: working towards a nuget package that stages, seems to pack dependencies now.

--- source/DevOps/release.sh
+++ source/DevOps/release.sh
@@ -23,21 +23,23 @@ echo -e "\e[35mRunning test for solution...\e[0m"
 for project in *.Tests/*.csproj; do 
 	echo -e "\e[33Executing Tests for $project\e[0m"
 	dotnet test --no-build $project  
+	
 	if [ $? -ne 0 ] 
 	then
 		echo -e "\e[31mTest Failure, Aborting!!!\e[0m"
-		git reset --hard
+		#git reset --hard
 		exit
 	fi
 done
 
+echo -e "\e[33mCreating NuGet Package\e[0m"
 if [ ! -d "$dist" ]; then
   mkdir $dist
 fi
 
-echo -e "\e[33mCreating NuGet Package\e[0m"
 cd $appdir
-dotnet pack -c Release --output ../$dist
+dotnet build -c release
+mv "Analyzer/bin/Release/GD3-Analyzer.$version.nupkg" $dist
 
 # commit build number bump to source
 cd ..
